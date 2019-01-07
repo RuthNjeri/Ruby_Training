982b63de74da4026ac93a78c1350bdf9cd243c60
RuthNjeri <ruthnwaiganjo@gmail.com>
2019-01-07 13:55:44 +0300

    epidocus tutorials

---
+++ epidocus_ruby/objects.rb
@@ -0,0 +1,15 @@
+# everything in Ruby is an object "hello" [1,2,3]
+# to get the object type irb in the terminal then "hello.class()" =>string
+# .class() is a method(function called on an object)
+# all objects are instances of a class
+# Interesting: 1+3 is passing an argument in a method and calling it on an object 1.+(3)
+# Ruby has a lot of syntactic sugar.. syntax that is sweet to the human eye, easier to read or express
+# Number Class and methods >5.even?() => false
+# >5.round() => 5
+# >6.4.ceil() =>7
+# you can chain methods together when one method returns an object  > 6.9.round().next() => 8
+# Arrays
+# [1,2,4].push(4) => [1,2,4,4]
+# [1,2,4].length() => 3
+# 
+#
